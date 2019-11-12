e6708289a13f5cadfd12f8131cf8fab4c1ff4fe4
Noel Ryan <noelgerard.ryan@gmail.com>
2019-11-12 15:05:41 +0100

    Creating ruby.rb file to shopw some basic ruby

---
+++ ruby.rb
@@ -0,0 +1,52 @@
+Variables 
+
+snake_case 
+my_variable
+
+color 
+
+Instance Variables - @Instance
+
+Global Variables - $Global 
+
+Local Variables - 
+
+
+Integars number 1 2 3 4
+
+'strings' "strings"
+
+"my_string #{1+1}"
+
+Boolean true or false
+
+floats 1.2 1.3 4.7
+
+Hashes ={ bottle:clear 'water'}
+
+Hashes[:key]
+
+Arrays = [1,true,2.3,'string']
+
+arrays[2]
+
+Symbols 
+
+:bottle 
+
+Class
+
+class Demo
+
+    
+end
+
+new_demo = Demo.new
+
+methods 
+
+def my_method(number)
+    1 + number
+end
+
+my_method(1)
